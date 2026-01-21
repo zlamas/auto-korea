e75b0cc362efa3e7edc4a06460688616e52353ee
Vadim I. <vadign00@gmail.com>
2026-01-21 18:38:20 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
                 about: 'about.html',
                 reviews: 'reviews.html',
                 services: 'services.html',
+                contact: 'contact.html',
             },
         },
     },
